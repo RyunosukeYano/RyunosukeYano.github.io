1059c9d043fd39922bf3a508ac73470636b15f13
RyunosukeYano <respsteve22@gmail.com>
2017-02-23 17:11:20 +0900

    First commit
    Add files

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:site_name" content="グローイングひまわりの種
+よっしーサプライズ" />
+	<title>グローイングひまわりの種
+よっしーサプライズ</title>
+	<link rel="stylesheet" type="text/css" href="css/slick.css">
+	<link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="js/slick.min.js"></script>
+</head>
+<body>
+	<header>
+		<p>ひまわりの種のお父さん! これからもよろしくね！</p>
+	</header>
+	<div class="slider">
+	  <div class="main_img"><img src="img/main_visual01.png">
+	  <h1>よっしー！お誕生日おめでとう！<br>素敵な1年にしてね！</h1>
+	  </div>
+	  <div class="main_img"><img src="img/main_visual04.png">
+	  <h1>よっしー！お誕生日おめでとう！<br>素敵な1年にしてね！</h1></div>
+	  <div class="main_img"><img src="img/main_visual03.png">
+	  <h1>よっしー！お誕生日おめでとう！<br>素敵な1年にしてね！</h1></div>
+	  <div class="main_img"><img src="img/main_visual02.png">
+	  <h1>よっしー！お誕生日おめでとう！<br>素敵な1年にしてね！</h1>
+	  </div>
+
+	</div>
+		<div class="section_title">
+			<img src="img/section_title.png">
+		</div>
+		<div class="main_section">
+			<div class="container">
+				<div class="img_cake wow fadeInUp"><video controls poster="img/message_plate_kahana.png" src="video/kahana-message.mp4"></video>
+				</div>
+				<div class="img_cake center_video"><video controls poster="img/message_plate_himawari.png" src="video/himawari-message.mp4"></video>
+				</div>
+				<div class="img_cake"><video controls poster="img/message_plate_yano.png" src="video/yano-message.mp4"></video>
+				</div>
+			</div>
+		</div>
+	<footer>
+		<p>Copyright ©ひまわりの種  All Rights Reserved.</p>
+	</footer>
+</body>
+<script>
+	$('.slider').slick({
+	// アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
+	accessibility: true,
+	// 自動再生。trueで自動再生される。
+	autoplay: true,
+	// 自動再生で切り替えをする時間
+	autoplaySpeed: 3000,
+	// スライドのエリアにマウスオーバーしている間、自動再生を止めるかどうか。
+  	pauseOnHover: false,
+	dots: true,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear'
+	});
+</script>
+</html>
